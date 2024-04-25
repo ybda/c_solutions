a6dd7f00fb4cbefc3153eb0093770972f776e035
Dmitriy B. <dbkkll@yandex.ru>
2024-04-25 12:14:06 +0500

    Add binarySearch & quicksort

---
+++ src/binarySearch.h
@@ -0,0 +1,19 @@
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+
+
+int mid, val;
+int binarySearch(int *arr, int l, int r, int num) {
+    if (l <= r) {
+        mid = (l + r) / 2;
+        val = arr[mid];
+        if (val > num) return binarySearch(arr, l, mid - 1, num);
+        if (val < num) return binarySearch(arr, mid + 1, r, num);
+        return mid;
+    }
+
+    return -1;
+}
+
+
+#endif // BINARY_SEARCH_H
---
+++ src/quicksort.h
@@ -0,0 +1,21 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+
+void quicksort(int *target, int left, int right) {
+    if(left >= right) return;
+    int i = left, j = right;
+    int tmp, pivot = target[i];
+    for(;;) {
+        while(target[i] < pivot) i++;
+        while(pivot < target[j]) j--;
+        if(i >= j) break;
+        tmp = target[i]; target[i] = target[j]; target[j] = tmp;
+        i++; j--;
+    }
+    quicksort(target, left, i-1);
+    quicksort(target, j+1, right);
+}
+
+
+#endif // QUICKSORT_H
